4ddc108bce99844e9c757beddf6b7afa0f764793
v-sreevidya <sreevidyavijayan14@gmail.com>
2024-12-27 23:50:55 +0530

    added responsiveness

--- src/Components/Marvel/Marvelstudios.jsx
+++ src/Components/Marvel/Marvelstudios.jsx
@@ -5,11 +5,7 @@ const Marvelstudios = () => {
   return (
     <div className="heading">
       <h3 className="name">MARVEL STUDIOS</h3>
-<<<<<<< HEAD
       <p className="discription">
-=======
-      <p className="description">
->>>>>>> fe2f714c85dc845c45a9427f39623f0cda758e01
         Marvel Studios is best known for creating the MCU, which is a shared 
         cinematic universe comprising a vast array of interconnected superhero films and television series.
         The MCU has been incredibly successful and includes popular characters like Iron Man,
