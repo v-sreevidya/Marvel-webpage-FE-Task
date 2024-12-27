d98c07e89ae97af891b9353f0e5315c74f3ef8c0
v-sreevidya <sreevidyavijayan14@gmail.com>
2024-12-27 23:07:56 +0530

    resolved responsiveness issues

---
+++ src/Components/Avengers/Avengers.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import './Avengers.css';
+const Avengers = () => {
+    return (
+        <div>
+            <h1 className="head">AVENGERS</h1>
+            <div className="thor"></div>
+            <div className="iron"></div>
+            <div className="captain"></div>
+            <div className="hulk"></div>
+            <div className="black"></div>
+            <div className="spider"></div>
+        </div>
+    );
+};
+
+export default Avengers;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="board">
+      <p className="quote">Reality is the stillness buried deep beneath the illusion</p>
+    </div>
+  );
+};
+
+export default Header;
